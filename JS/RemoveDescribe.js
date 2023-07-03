3a68d25ac8eea5542f07a96819cca3643f59e7a2
ZSXHH <127708283+ZSXHH@users.noreply.github.com>
2023-07-03 22:50:47 +0800

    Update RemoveDescribe.js

--- JS/RemoveDescribe.js
+++ JS/RemoveDescribe.js
@@ -6,15 +6,15 @@ else{
 var res = $response.body;
     console.log('原内容：\n'+res);
 //去多余
-if($persistentStore.read(AuthHP) == 1){
+if($persistentStore.read('AuthHP') == 1){
    res = res.replace(/\#\!(author|homepage|icon)\s?=.*/g,'#!$1 =');
 }
 
-if($persistentStore.read(Icon) == 1){
+if($persistentStore.read('Icon') == 1){
    res = res.replace(/\#\!icon\s?=.*/g,'#!$1 =');
 }
 
-if($persistentStore.read(Desc) == 1){
+if($persistentStore.read('Desc') == 1){
 var res = res.replace(/(\#\!desc\s*=.{0,18}).*/g,'$1');
 }
 
