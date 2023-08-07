aae6eac3363b72c17a9db6759eb4495808c2c536
ZSXHH <127708283+ZSXHH@users.noreply.github.com>
2023-08-08 05:34:43 +0800

    Update RemoveDescribe.js

--- JS/RemoveDescribe.js
+++ JS/RemoveDescribe.js
@@ -18,12 +18,30 @@ if($persistentStore.read('Desc') == 1){
 var res = res.replace(/(\#\!desc\s*=.{0,18}).*/g,'$1');
 }
 
-var jsna = $persistentStore.read('JSN').split('&');
-  for(j=0;j<5;j++){
-    
-    var jsnp = jsna[j] + '.js,';
-         res = res.replace(jsnp,jsnp+'enabled = false,');
-      if(res.includes(jsnp + 'enabled = false,')){break;}
-  }
+//新增去除插件内规则、关闭指定脚本
+var Rname = $request.url.match(/[\/]+(?=\.plugin)/)[0];
+     console.log('提取链接名：'+Rname);
+for(m=1;m<3;m++){
+   var RName = $persistentStore.read('Name-'+m).match(/^[^\&]+/)[0];
+     console.log('匹配插件名：'+RName);
+ if(RName==Rname){
+   var Rarr = $persistentStore.read('Name-'+m).split('&'); 
+     console.log('项目数组：'+Rarr);
+           break;}
+                }
+if(typeof Rarr  === 'undefined'){
+   console.log('无需去除规则或脚本');
+$done({body:res});
+}
+  
+for(k=1;k<Rarr.length;k++){
+  if(Rarr[k]=='r'){
+    var res = res.replace(/\[Rule\]\n[^\[]+/,'');
+console.log('去规则：'+res);continue;
+                  }
+    var Rarrjs = Rarr[k] + '.js,';
+    var res = res.replace(Rarrjs,Rarrjs+'enabled = false,');
+console.log('去脚本：'+res);
+}
 $done({body:res})
 }
